81afa18ca1d78d8e650ec0d7dd89dcf543b3f5da
Andrea Recine <148541295+andrerecio@users.noreply.github.com>
2026-07-08 21:15:08 +0200

    Translate Tutorials 6 and 7

--- tutorial6/tutorial6_script.R
+++ tutorial6/tutorial6_script.R
@@ -1,4 +1,4 @@
-# Esercitazione N.6 - Linear Probability Model (LPM)
+# Tutorial 6 - Linear Probability Model (LPM)
 # Andrea Recine
 
 
@@ -13,15 +13,15 @@ library(wooldridge)
 
 # Dataset alcohol --------------------------------------------------------------
 
-# Carichiamo i dati alcohol dal pacchetto wooldridge
+# Load the alcohol data set from the wooldridge package
 data("alcohol", package = "wooldridge")
 head(alcohol)
 
-# Statistiche descrittive
+# Descriptive statistics
 datasummary_skim(alcohol)
 
-# Stima del LPM: employ ~ abuse + educ + age + married
-# Errori standard robusti all'eteroschedasticità (intrinseca nel LPM)
+# Estimate the LPM: employ ~ abuse + educ + age + married
+# Heteroskedasticity-robust standard errors (heteroskedasticity is inherent in the LPM)
 reg_lpm_alcohol <- feols(employ ~ abuse + educ + age + married,
                          data = alcohol,
                          vcov = "hetero")
@@ -29,32 +29,32 @@ modelsummary(list("Employ" = reg_lpm_alcohol),
              gof_omit = "AIC|BIC|RMSE|R2 Adj.")
 
 
-# Probabilità predette ---------------------------------------------------------
+# Predicted probabilities -----------------------------------------------------
 
-# Uomo di 30 anni, sposato, 12 anni di istruzione, non abusa di alcol
+# 30-year-old married man, 12 years of education, does not abuse alcohol
 predict(reg_lpm_alcohol,
         newdata = data.frame(abuse = 0, educ = 12, age = 30, married = 1))
 
-# Uomo di 30 anni, non sposato, 10 anni di istruzione, abusa di alcol
+# 30-year-old unmarried man, 10 years of education, abuses alcohol
 predict(reg_lpm_alcohol,
         newdata = data.frame(abuse = 1, educ = 10, age = 30, married = 0))
 
 
-# Dataset crime1 - Probabilità di essere arrestato -----------------------------
+# crime1 data set - Probability of being arrested -----------------------------
 
-# Carichiamo i dati crime1 dal pacchetto wooldridge
+# Load the crime1 data set from the wooldridge package
 data("crime1", package = "wooldridge")
 head(crime1)
 
-# Creazione della dummy 'arrested': 1 se l'individuo è stato arrestato
-# almeno una volta nel 1986, 0 altrimenti
+# Create the 'arrested' dummy: 1 if the individual was arrested
+# at least once in 1986, 0 otherwise
 crime1 <- crime1 %>%
   mutate(arrested = ifelse(narr86 > 0, 1, 0))
 
-# Stima del LPM con errori standard robusti all'eteroschedasticità
-# Categoria omessa per le dummy etniche: uomini bianchi/altra etnia
-# (black = 0 e hispan = 0). I coefficienti di black e hispan si leggono
-# rispetto a questo gruppo di riferimento.
+# Estimate the LPM with heteroskedasticity-robust standard errors
+# Omitted category for the ethnicity dummies: White men/other ethnicity
+# (black = 0 and hispan = 0). The coefficients on black and hispan are interpreted
+# relative to this reference group.
 reg_lpm_crime <- feols(arrested ~ pcnv + avgsen + tottime + ptime86 + qemp86 + black + hispan,
                        data = crime1,
                        vcov = "hetero")
